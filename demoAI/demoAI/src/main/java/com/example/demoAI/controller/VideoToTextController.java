5a6ccfa902d2b06acb36a1b980a912ddbcf225c1
MinhQuang <minhquang10ks@gmail.com>
2024-03-25 23:33:30 +0700

    update api chat open ai

--- demoAI/demoAI/src/main/java/com/example/demoAI/controller/VideoToTextController.java
+++ demoAI/demoAI/src/main/java/com/example/demoAI/controller/VideoToTextController.java
@@ -4,88 +4,89 @@ import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.api.gax.longrunning.OperationFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.speech.v1.*;
+import com.google.cloud.texttospeech.v1.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.google.protobuf.ByteString;
 import io.grpc.Context;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import com.google.cloud.texttospeech.v1.*;
+import com.google.cloud.texttospeech.v1.SynthesisInput;
+import com.google.cloud.texttospeech.v1.AudioConfig;
+import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.ExecutionException;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.*;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Path;
 
 @RestController
 public class VideoToTextController {
 
     @PostMapping("/api/convert")
     public String convertVideoToText(@RequestParam("file") MultipartFile file) {
-        // Kiểm tra xem tệp đã được tải lên hay không
         if (file.isEmpty()) {
             return "Vui lòng chọn một tệp video.";
         }
 
         try {
-            // Tạo đường dẫn tạm thời cho tệp video
-            Path tempFilePath = Files.createTempFile("temp", ".mp3");
+            Path tempFilePath = Files.createTempFile("temp", ".mp4");
             file.transferTo(tempFilePath.toFile());
 
-            // Tạo URI của file trên Google Cloud Storage (GCS)
             String gcsUri = uploadToGCS(tempFilePath);
 
-            // Tạo Credentials Provider từ key file
             GoogleCredentials credentials = GoogleCredentials.fromStream(
                     new FileInputStream("./demoAI/src/main/resources/keyfile1.json"));
             FixedCredentialsProvider credentialsProvider = FixedCredentialsProvider.create(credentials);
-
-            // Tạo cài đặt cho SpeechClient
             SpeechSettings settings = SpeechSettings.newBuilder()
                     .setCredentialsProvider(credentialsProvider)
                     .build();
 
-            // Khởi tạo SpeechClient
             try (SpeechClient speechClient = SpeechClient.create(settings)) {
-                // Đọc dữ liệu từ tệp và chuyển đổi thành ByteString
-                byte[] data = Files.readAllBytes(tempFilePath);
-                ByteString audioBytes = ByteString.copyFrom(data);
-
-                // Tạo RecognitionConfig
                 RecognitionConfig config = RecognitionConfig.newBuilder()
-                        .setEncoding(RecognitionConfig.AudioEncoding.MP3)
+                        .setEnableAutomaticPunctuation(true)
+                        .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                        .setSampleRateHertz(48000)
                         .setLanguageCode("vi-VN")
+                        .setModel(RecognitionConfig.getDefaultInstance().getModel())
                         .build();
 
-                // Tạo RecognitionAudio từ URI của file trên GCS
                 RecognitionAudio audio = RecognitionAudio.newBuilder()
                         .setUri(gcsUri)
                         .build();
 
-
-                // Gửi yêu cầu bất đồng bộ đến Google Cloud Speech-to-Text API và nhận kết quả
                 OperationFuture<LongRunningRecognizeResponse, LongRunningRecognizeMetadata> response = speechClient.longRunningRecognizeAsync(config, audio);
-
-                // Chờ cho đến khi quá trình nhận diện hoàn tất và nhận kết quả
                 LongRunningRecognizeResponse longRunningResponse = response.get();
 
-                // Trích xuất văn bản từ kết quả
                 StringBuilder transcriptBuilder = new StringBuilder();
                 for (SpeechRecognitionResult result : longRunningResponse.getResultsList()) {
-                    // Lấy văn bản từ mỗi kết quả
                     String transcript = result.getAlternatives(0).getTranscript();
                     transcriptBuilder.append(transcript).append("\n");
                 }
 
-                // Xóa tệp tạm thời
                 Files.deleteIfExists(tempFilePath);
-
-                // Trả về văn bản trích xuất từ video
                 return transcriptBuilder.toString();
             }
         } catch (IOException | InterruptedException | ExecutionException e) {
@@ -96,38 +97,96 @@ public class VideoToTextController {
 
     private String uploadToGCS(Path filePath) {
         try {
-            // Lấy tên file từ đường dẫn
             String fileName = filePath.getFileName().toString();
-
-            // Tải file keyfile.json để xác thực với GCS
             GoogleCredentials credentials = GoogleCredentials.fromStream(
                     new FileInputStream("./demoAI/src/main/resources/keyfile1.json"));
-
-            // Khởi tạo client của GCS
             Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-
-            // Tạo tên bucket và đường dẫn đến file trên GCS
             String bucketName = "aispeech2text";
-            String objectName = fileName; // Sử dụng tên file từ đường dẫn
+            String objectName = fileName;
 
-            // Tạo bucket nếu chưa tồn tại
             Bucket bucket = storage.get(bucketName);
             if (bucket == null) {
                 bucket = storage.create(BucketInfo.of(bucketName));
             }
 
-            // Tạo file object từ đường dẫn tới file trên local file system
             BlobId blobId = BlobId.of(bucketName, objectName);
             BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
 
-            // Tải file lên GCS
             Blob blob = storage.create(blobInfo, Files.readAllBytes(filePath));
 
-            // Trả về URI của file trên GCS
             return "gs://" + bucketName + "/" + objectName;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
+
+//    private static final String API_KEY = "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";
+//    private static final String API_KEY = "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";
+    private static final String API_KEY = "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";
+
+    @PostMapping("/api/chat")
+    public ResponseEntity<String> chatCompletion(@RequestBody String userMessage) {
+        try {
+            String completion = getChatCompletion(userMessage);
+            return ResponseEntity.ok(completion);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing request");
+        }
+    }
+
+    private String getChatCompletion(String userMessage) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("https://api.openai.com/v1/chat/completions");
+
+        // Add headers
+        httpPost.addHeader("Content-Type", "application/json");
+        httpPost.addHeader("Authorization", "Bearer " + API_KEY);
+
+        // Set request body using Gson
+        Gson gson = new Gson();
+        JsonObject requestBodyJson = new JsonObject();
+        requestBodyJson.addProperty("model", "gpt-3.5-turbo");
+        JsonArray messagesArray = new JsonArray();
+        JsonObject messageObject = new JsonObject();
+        messageObject.addProperty("role", "user");
+//        messageObject.addProperty("language", "vi");
+//        messageObject.addProperty("content", userMessage + "| Notes: Please summarize as follows:\n" +
+//                "- time\n" +
+//                "- location\n" +
+//                "- Content\n" +
+//                "- things need to notice\n" +
+//                "And other meanings");
+        messageObject.addProperty("content", userMessage + "| Notes:Please summarize as follows:\n" +
+                "- Title\n" +
+                "- Time\n" +
+                "- Location\n" +
+                "- Content\n" +
+                "- Things to pay attention to\n" +
+                "and other content ideas. Please return the results to me as a html snippet with css that bolds the title, Time, Content, Location, Things to note and in front of each internal content with a - at the beginning.");
+//        messageObject.addProperty("content", userMessage + "| Notes: Please give suggestions on what needs to be done or can be done, and how to solve the problem, if any, based on this text.");
+        messagesArray.add(messageObject);
+        requestBodyJson.add("messages", messagesArray);
+
+        requestBodyJson.addProperty("temperature", 0.7);
+        String requestBody = gson.toJson(requestBodyJson);
+        httpPost.setEntity(new StringEntity(requestBody));
+
+        // Execute the request
+        HttpResponse response = httpClient.execute(httpPost);
+        HttpEntity entity = response.getEntity();
+
+        // Process the response
+        String result = null;
+        if (entity != null) {
+            result = EntityUtils.toString(entity);
+        }
+
+        // Close the HttpClient
+        httpClient.close();
+
+        return result;
+    }
+
 }
